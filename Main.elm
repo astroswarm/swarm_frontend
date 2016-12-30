c68ca2bb9a3497918ad754fe75961225072cdb30
Robby Grossman <robby@freerobby.com>
2016-12-30 00:02:20 -0500

    Implement Model-View pattern.

--- Main.elm
+++ Main.elm
@@ -4,17 +4,49 @@ import Html
 import Html.Attributes
 
 -- Model
+type alias Model =
+  {
+    services: List Service,
+    selected_service_index: Int
+  }
+type alias Service =
+  {
+    name: String,
+    port_offset: Int
+  }
+
+initialModel : Model
+initialModel =
+  {
+    services =
+      [
+        {
+          name = "Lin Guider (Autoguider)",
+          port_offset = 2
+        },
+        {
+          name = "PHD2 (Autoguider)",
+          port_offset = 1
+        },
+        {
+          name = "Open Sky Imager (Camera Controller)",
+          port_offset = 3
+        }
+      ],
+     selected_service_index = -1
+  }
 
 -- Update
 
 -- View
 view =
   Html.div [ Html.Attributes.class "container" ] [
-    Html.ul [ Html.Attributes.class "collection" ] [
-      Html.li [ Html.Attributes.class "collection-item" ] [ Html.text "PHD2 (Autoguider)" ],
-      Html.li [ Html.Attributes.class "collection-item" ] [ Html.text "Lin Guider (Autoguider)" ],
-      Html.li [ Html.Attributes.class "collection-item" ] [ Html.text "Open Sky Imager (Camera Control)"]
-    ]
+    Html.ul [ Html.Attributes.class "collection" ] (
+      List.map (\
+        service ->
+          Html.li [ Html.Attributes.class "collection-item" ] [ Html.text service.name ]
+      ) initialModel.services
+    )
   ]
 
 main =
