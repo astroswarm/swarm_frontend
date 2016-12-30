cdf75299fea2b0e6dcf45d9752e60a1300f84d25
Robby Grossman <robby@freerobby.com>
2016-12-29 23:15:32 -0500

    Generate static list of controllable applications.

--- Main.elm
+++ Main.elm
@@ -1,3 +1,21 @@
-import Html
+-- Imports
 
-main = Html.text "Hello world"
+import Html
+import Html.Attributes
+
+-- Model
+
+-- Update
+
+-- View
+view =
+  Html.div [ Html.Attributes.class "container" ] [
+    Html.ul [ Html.Attributes.class "collection" ] [
+      Html.li [ Html.Attributes.class "collection-item" ] [ Html.text "PHD2 (Autoguider)" ],
+      Html.li [ Html.Attributes.class "collection-item" ] [ Html.text "Lin Guider (Autoguider)" ],
+      Html.li [ Html.Attributes.class "collection-item" ] [ Html.text "Open Sky Imager (Camera Control)"]
+    ]
+  ]
+
+main =
+  view
