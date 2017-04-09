548c51a11f95e144651080dfda86116a2d72e20a
Robby Grossman <robby@freerobby.com>
2017-04-09 00:49:49 -0400

    Add support for uploading of system logs.

--- Main.elm
+++ Main.elm
@@ -3,6 +3,9 @@
 import Html
 import Html.Attributes
 import Html.Events
+import Http
+import Json.Decode
+import Json.Encode
 import Maybe
 
 -- Model
@@ -20,7 +23,8 @@ type alias Model =
   {
     services: List Service,
     selected_service_name: String,
-    hostname: String
+    hostname: String,
+    uploaded_log_url: String
   }
 
 -- Model Initialization
@@ -36,14 +40,15 @@ init {hostname} =
           Service "Open Sky Imager (Camera Controller)" 6103
         ],
        selected_service_name = "Lin Guider (Autoguider)",
-       hostname = hostname
+       hostname = hostname,
+       uploaded_log_url = ""
     },
     Cmd.none
   )
 
 -- Update
 
-type Msg = NoOp | ServiceSelect String
+type Msg = NoOp | ServiceSelect String | UploadLogs | LogsUploaded (Result Http.Error String)
 
 update: Msg -> Model -> (Model, Cmd Msg)
 
@@ -53,6 +58,43 @@ update message model =
       (model, Cmd.none)
     ServiceSelect new_service ->
       ({ model | selected_service_name = new_service }, Cmd.none)
+    UploadLogs ->
+      (model, uploadLogs)
+    LogsUploaded (Ok output) ->
+      let
+        url =
+          output
+          |> String.filter (\c -> c /= '\n')
+      in
+        ({ model | uploaded_log_url = url }, Cmd.none)
+    LogsUploaded (Err error) ->
+      ({ model | uploaded_log_url = (toString error) }, Cmd.none)
+
+
+-- COMMANDS
+
+uploadLogs : Cmd Msg
+uploadLogs =
+  let
+    body = Json.Encode.object [
+      ("command", Json.Encode.string("pastebinit")),
+      ("args", Json.Encode.list(
+        [
+          Json.Encode.string("-b"),
+          Json.Encode.string("sprunge.us"),
+          Json.Encode.string("/mnt/host/var/log/syslog")
+        ]
+      ))]
+      |> Http.jsonBody
+    url = "http://localhost:8001/api/execute_command"
+  in
+    Http.post url body logsUploadedDecoder
+      |> Http.send LogsUploaded
+
+
+logsUploadedDecoder : Json.Decode.Decoder String
+logsUploadedDecoder =
+  Json.Decode.field "output" Json.Decode.string
 
 
 -- VIEW
@@ -85,6 +127,24 @@ view model =
         Html.p [ ] [ Html.text ("Using host: " ++ model.hostname) ]
       ]
 
+    viewUploadLogs =
+      Html.p [ ] [
+        Html.a [
+          Html.Attributes.href "javascript: return false;",
+          Html.Events.onClick UploadLogs
+        ] [ Html.text "Having trouble? Click here to upload your logs." ],
+        if String.length(model.uploaded_log_url) > 0 then
+          Html.p [ ] [
+            Html.text "Your logs have been uploaded: ",
+            Html.a [
+              Html.Attributes.href model.uploaded_log_url,
+              Html.Attributes.target "_blank"
+            ] [ Html.text model.uploaded_log_url ]
+          ]
+        else
+          Html.text ""
+      ]
+
 
     viewServiceEmbed =
       Html.iframe [
@@ -104,6 +164,7 @@ view model =
     Html.div [ Html.Attributes.class "container" ] [
       viewServicesList,
       viewStatusInfo,
+      viewUploadLogs,
       viewServiceEmbed
     ]
 
